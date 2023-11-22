35e238394f1011be549d153908799dd86e3578ae
Tarbiee <dorcusstaby@gmail.com>
2023-11-22 14:47:25 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list =[]
+
+    if length >= 1:
+        my_list.append(0)
+    if length >= 2:
+        my_list.append(1)
+    for n in range(2,length):
+        next_number = my_list[-1] + my_list[-2]
+        my_list.append(next_number)
+        
+    print(my_list)
+print_fibonacci(9)
